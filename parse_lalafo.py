ea2afd89f782e0869af3390a804dcd659afd6799
farkhatzhan-makhmudov <faralost@gmail.com>
2022-09-06 13:56:33 +0600

    add function request for lalafo detail data for one item

--- parse_lalafo.py
+++ parse_lalafo.py
@@ -17,3 +17,17 @@ def get_json_for_rental_estate_items_by_category(per_page, category_id):
         return response_data
     except httpx.RequestError as exc:
         print(f"An error occurred while requesting {exc.request.url!r}.")
+
+
+def get_json_for_item_details(item_id):
+    url = f'https://lalafo.kg/api/search/v3/feed/details/{item_id}'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "device": "pc",
+    }
+    try:
+        response = httpx.get(url, headers=headers)
+        return response.json()
+    except httpx.RequestError as exc:
+        print(f"An error occurred while requesting {exc.request.url!r}.")
