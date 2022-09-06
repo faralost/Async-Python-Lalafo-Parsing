999cd99643c344c7f2323f41a7bf51a1f5770e44
farkhatzhan-makhmudov <faralost@gmail.com>
2022-09-06 13:52:54 +0600

    add function request for lalafo data by category

---
+++ parse_lalafo.py
@@ -0,0 +1,19 @@
+import httpx
+
+
+def get_json_for_rental_estate_items_by_category(per_page, category_id):
+    url = f'https://lalafo.kg/api/search/v3/feed/search?expand=url&per-page={per_page}&category_id={category_id}'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "device": "pc",
+    }
+    params = {
+        'sort_by': 'newest'
+    }
+    try:
+        response = httpx.get(url, headers=headers, params=params)
+        response_data = response.json()
+        return response_data
+    except httpx.RequestError as exc:
+        print(f"An error occurred while requesting {exc.request.url!r}.")
